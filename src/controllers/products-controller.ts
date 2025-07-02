9da4ba86bc3b0e8aa5b21d4e6156cda942a7fdbb
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-07-01 21:01:03 -0300

    add zod / products validation

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,12 +1,27 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "@/utils/AppError";
+import { z } from "zod"
 
 class ProductsController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      throw new AppError("Erro de teste")
-
+      // throw new AppError("Erro de teste")
       return response.json({ message: "Ok" })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      // const { name, price } = request.body
+      const bodySchema = z.object({
+        name: z.string({ required_error: "name is required!" }).trim().min(3),
+        price: z.number({ required_error: "price is required!"}).gt(0) // gt(0, { message: "value must be greater than 0"})-alternativa, pois retorna mensagem similar
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      return response.status(201).json({ name, price })
     } catch (error) {
       next(error)
     }
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,9 +1,19 @@
 import { Request, Response, NextFunction } from "express"
 import { AppError } from "@/utils/AppError";
+import { ZodError } from "zod"
 
-export function errorHandling(error: any, request: Request, response: Response, _: NextFunction) {
+export function errorHandling(
+  error: any,
+  request: Request, 
+  response: Response, 
+  _: NextFunction
+) {
   if (error instanceof AppError) {
     response.status(error.statusCode).json({ message: error.message })
+  }
+ 
+  if (error instanceof ZodError) {
+    response.status(400).json({ message: "validation error", issues: error.format() })
   }
 
   return response.status(500).json({ message: error.message })
